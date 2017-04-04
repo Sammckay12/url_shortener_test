f989b7273f3246d05b95e1182fb4c411414a025a
Sam Mckay <sammckay12@gmail.com>
2017-04-04 15:24:13 +0100

    added schema for db

--- models/url.js
+++ models/url.js
@@ -1,2 +1,15 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+
+var CounterSchema = Schema({
+    _id: {type: String, required: true},
+    seq: { type: Number, default: 0 }
+});
+
+var counter = mongoose.model('counter', CounterSchema);
+
+var urlSchema = new Schema({
+  _id: {type: Number, index: true},
+  long_url: String,
+  created_at: Date
+});
