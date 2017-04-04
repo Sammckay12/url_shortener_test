8e941da5c5aaf83bb1d661e9b22ee07898884ca0
Sam Mckay <sammckay12@gmail.com>
2017-04-04 16:15:19 +0100

    installed body-parser to to submit form and return shortened url

--- models/url.js
+++ models/url.js
@@ -13,3 +13,21 @@ var urlSchema = new Schema({
   long_url: String,
   created_at: Date
 });
+
+// The pre('save', callback) middleware executes the callback function
+// every time before an entry is saved to the urls collection.
+urlSchema.pre('save', function(next){
+  var doc = this;
+  // find the url_count and increment it by 1
+  counter.findByIdAndUpdate({_id: 'url_count'}, {$inc: {seq: 1} }, function(error, counter) {
+      if (error)
+          return next(error);
+      // set the _id of the urls collection to the incremented value of the counter
+      doc._id = counter.seq;
+      doc.created_at = new Date();
+      next();
+  });
+});
+
+var Url = mongoose.model('Url', urlSchema);
+module.exports = Url;
