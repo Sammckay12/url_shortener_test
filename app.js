a6386251803d614131e4249c0bcb4715b8b223ae
Sam Mckay <sammckay12@gmail.com>
2017-04-06 16:59:43 +0100

    changed CORS function

--- app.js
+++ app.js
@@ -16,7 +16,13 @@ mongoose.connect(mongoUrl);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(cors());
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 
