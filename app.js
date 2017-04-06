76bf695433cff92225237f70474482da1b620253
Sam Mckay <sammckay12@gmail.com>
2017-04-06 16:43:22 +0100

    trying to solve CORS issues try 2

--- app.js
+++ app.js
@@ -16,7 +16,23 @@ mongoose.connect(mongoUrl);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(cors({credentials: true, origin: true}));
+var whitelist = [
+    'http://localhost:3000',
+];
+var corsOptions = {
+    origin: function(origin, callback){
+        var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
+        callback(null, originIsWhitelisted);
+    },
+    credentials: true
+};
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
+app.use(cors(corsOptions));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
