078299c2b66eaa5821a6690170b7112d0b30fc08
Sam Mckay <sammckay12@gmail.com>
2017-04-07 12:17:55 +0100

    url generated amendment

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var config = {};
 const ENV = 'PROD'
 
 config.db = {};
-config.webhost = ENV === 'DEV' ? 'http://localhost:3000/' : 'https://node-spikely.herokuapp.com'
+config.webhost = ENV === 'DEV' ? 'http://localhost:3000/' : 'https://node-spikely.herokuapp.com/'
 
 config.db.host = 'localhost';
 config.db.name = 'url_shortener';
